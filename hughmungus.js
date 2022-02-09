e282b2b83c739b2794e5ee4896a300b9e1310e6b
Nate <nathantrimble1@gmail.com>
2022-02-09 09:55:17 -0800

    buttons (oatmeal doesnt work)

--- hughmungus.js
+++ hughmungus.js
@@ -1,25 +1,21 @@
 $(document).ready(function(){
   $("#banana").click(function(){
     $("#bananaS").toggle();
-
+    });
     $("#oatmeal").click(function(){
       $("#oatmealS").toggle();
-
+      });
       $("#eggs").click(function(){
         $("#eggS").toggle();
-
+        });
         $("#broccoli").click(function(){
           $("#broccoliS").toggle();
-
+          });
           $("#chicken").click(function(){
             $("#chickenS").toggle();
-
+            });
             $("#brownrice").click(function(){
               $("#brownriceS").toggle();
             });
-          });
-        });
-      });
-    });
-  });
+
 });
