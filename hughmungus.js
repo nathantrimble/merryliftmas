4df82783f8aaf2a3d519e8352111b738cd7a2795
christopherGraner <chrisgraner1@gmail.com>
2022-02-09 09:43:53 -0800

    background + other

--- hughmungus.js
+++ hughmungus.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
+  $("#banana").click(function(){
+    $("#bananaS").toggle();
 
+    $("#oatmeal").click(function(){
+      $("#oatmealS").toggle();
+
+      $("#eggs").click(function(){
+        $("#eggS").toggle();
+
+        $("#broccoli").click(function(){
+          $("#broccoliS").toggle();
+
+          $("#chicken").click(function(){
+            $("#chickenS").toggle();
+
+            $("#brownrice").click(function(){
+              $("#brownriceS").toggle();
+            });
+          });
+        });
+      });
+    });
+  });
 });
